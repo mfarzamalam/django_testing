add018dec246ef1859dfec349518b2fbc665753e
mfarzamalam <mfarzamalam@gmail.com>
2022-06-17 19:31:31 +0500

    added author table and some test first_name label

--- app_app/admin.py
+++ app_app/admin.py
@@ -1,3 +1,6 @@
+from re import A
 from django.contrib import admin
+from .models import Author
 
 # Register your models here.
+admin.site.register(Author)
--- app_app/tests.py
+++ app_app/tests.py
@@ -1,13 +1,28 @@
+import imp
 from django.test import TestCase
+from .models import Author
 
 # Create your tests here.
 
-class MyTestCase(TestCase):
+class MyDummyTestCase(TestCase):
     
     def test_false_is_false(self):
-        print("Method: test_false_is_false.")
+        print("METHOD: test_false_is_false.")
         self.assertFalse(False)
 
     def test_true_is_true(self):
-        print("Method: test_true_is_true.")
+        print("METHOD: test_true_is_true.")
         self.assertTrue(True)
+
+
+class ModelAuthorTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(self):
+        Author.objects.create(first_name='Big', last_name='Bob')
+
+    def test_first_name_label(self):
+        print("METHOD: test_first_name_label")
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEqual(field_label, 'first name')
