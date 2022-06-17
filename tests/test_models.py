8fb93315d21578171091fc2c5ac3ae3eb0c7afd6
mfarzamalam <mfarzamalam@gmail.com>
2022-06-17 19:18:04 +0500

    added new test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,10 +4,13 @@ from django.test import TestCase
 
 class MyTestCase(TestCase):
     
+    # @classmethod
+    # def setUpTestData(cls):
+    #     print("setUpTestData")
+
+    # def setUp(self):
+    #     print("setUp")
+
     def test_false_is_false(self):
         print("Method: test_false_is_false.")
         self.assertFalse(False)
-
-    def test_true_is_true(self):
-        print("Method: test_true_is_true.")
-        self.assertTrue(True)
